5213d75e93fab9a59c6fff7580f37f336ffc3c18
mfsimsek <mfsimsek@buffalo.edu>
2023-03-03 16:30:40 -0500

    Update posttranslationalclockonfgf_Simseketal_sourcecode.m

--- posttranslationalclockonfgf_Simseketal_sourcecode.m
+++ posttranslationalclockonfgf_Simseketal_sourcecode.m
@@ -7,6 +7,7 @@ delt=0.02;% temporal step size in mins
 delx=1;% spatial step size in cell number
 N=T/delt; % total no. of iteration steps
 xsize=1:40; % cell numbers
+tb=6;
 L=length(xsize); % length of the tissue in terms of cell no.
 tcell=6; % time in mins at which a cell is added in the tissue
 
@@ -22,21 +23,21 @@ D= 1; % diffusion const
 psM=10; % protein synthesis rate of Mek
 paC = 0.05; % activation rate of Mek
 pdC = 0.5; % degradation rate of Mek
-mdL =0.5; % Fgf RNA Decay Rate
+mdL =0.08; % Fgf RNA Decay Rate
 psL = 1.5; % protein synthesis rate of Fgf
 pdL = 0.4; % protein degradation rate of Fgf
-psD6=0.1; % protein synthesis rate of Dusp6
+psD6=0.05; % protein synthesis rate of Dusp6
 pdD6=0.6; % protein degradation rate of Dusp6
 pdD4=0.5; % protein degradation rate of Dusp4
 psE=80; % protein synthesis rate of Erk
 pdE=0.2; %protein degradation rate of Erk
 pdPE=pdE; %protein degradation rate of pErk
-pE=1*0.1; % phosphorylation rate of Erk
-dpE=10;% dephosphorylation rate of pErk 
+pE=0.1; % phosphorylation rate of Erk
+dpE=5;% dephosphorylation rate of pErk 
 da=1.0; % Association rate of clock and Dusp
 dd=0.1; % dissociation rate of clock and Dusp
 beta=0.2; % ratio of the degradation rate of clock-bound Dusp to free Dusp
-fb=0.5; % feedback coefficient from pErk to pMek
+piC=0.1; % feedback coefficient for pMek
 
 
 TdelML=10; % tranlational time-delay of Fgf
@@ -59,7 +60,7 @@ Erk=zeros(L,N);% Erk
 PErk=zeros(L,N);% pErk
 Dusp4f=zeros(L,N);% Free Dusp4
 Dusp4b=zeros(L,N);% clock-bound Dusp4
-Dusp6f=zeros(L,N);% Free Dyusp6
+Dusp6f=zeros(L,N);% Free Dusp6
 Dusp6b=zeros(L,N);% clock-bound Dusp6
 phi=zeros(L,N); % clock phase
 l=round(T/tcell+5);
@@ -68,6 +69,8 @@ PErk_lf=zeros(L+l,N);% pErk in the lab-frame
 
 msL=exp(-xsize/1);msL=msL/max(msL);
 msL=3*msL;
+msL=0*xsize;
+msL(1:tb)=3;
 f=1./(1+exp(5*(xsize-35)/L));f=2*pi/30*f; % frequnecy profile of the clock in the tissue
 psD4=1*m;% protein synthesis rate of Dusp4
 i=1;
@@ -119,8 +122,8 @@ for t=0:delt:T
     
     if i> idelML && i>idelact
         pLIG(:,i+1)=pLIG(:,i)+delt*(mLIG(:,i-idelML)*psL-pLIG(:,i)*pdL+diffusion(:));
-        COMP(:,i+1)=COMP(:,i)+delt*(psM-pLIG(:,i-idelact).*COMP(:,i)*paC-COMP(:,i)*pdC+fb*PErk(:,i).*PCOMP(:,i));
-        PCOMP(:,i+1)=PCOMP(:,i)+delt*(pLIG(:,i-idelact).*COMP(:,i)*paC-fb*PErk(:,i).*PCOMP(:,i)-pdC*PCOMP(:,i));
+        COMP(:,i+1)=COMP(:,i)+delt*(psM-pLIG(:,i-idelact).*COMP(:,i)*paC-COMP(:,i)*pdC+piC*PCOMP(:,i));
+        PCOMP(:,i+1)=PCOMP(:,i)+delt*(pLIG(:,i-idelact).*COMP(:,i)*paC-piC*PCOMP(:,i)-pdC*PCOMP(:,i));
         PCOMP(PCOMP<0)=0;
         COMP(COMP<0)=0;
     else
@@ -156,24 +159,136 @@ yD4=max(max(Dusp4f+Dusp4b));
 yD6=max(max(Dusp6f+Dusp6b));
 yC=max(max(Tclock));
 yM=max(max(PCOMP));
-offs=35;
-SFC=PErk_mf;
-for i=1:size(PErk_mf,2)
-    for j=2:size(PErk_mf,1)
-    SFC(j,i)=(PErk_mf(j-1,i)-max(PErk_mf(offs:40,i)))/(PErk_mf(j,i)-max(PErk_mf(offs:40,i)))-1;
-    end
-    SFC(1,i)=SFC(2,i);
-    siz=size(SFC(SFC(:,i)<=0,i));
-    nans=NaN(siz);
-    SFC(SFC(:,i)<=0,i)=nans;
-end
 
+% create snapshots from dynamics
 %{
+
 mf=6500;
-stp=330;
+stp=1000;
 figure
 
-plot(Tclock(2:end,mf))
+plot(Tclock(2:end,mf),'o','LineWidth',2,...
+    'MarkerSize',10,...
+    'MarkerEdgeColor','r',...
+    'MarkerFaceColor',[1.0,0.4,0.4])
+ax = gca;
+ax.FontSize = 30;
+ax.FontWeight='bold';
+xlabel('PSM (cells)','FontSize',40);
+ylabel('Clock (a.u.)','FontSize',40, 'Color', 'r', 'FontWeight', 'bold');
+
+figure
+plot(Tclock(2:end,mf+stp),'o','LineWidth',2,...
+    'MarkerSize',10,...
+    'MarkerEdgeColor','r',...
+    'MarkerFaceColor',[1.0,0.1,0.1])
+ax = gca;
+ax.FontSize = 30;
+ax.FontWeight='bold';
+xlabel('PSM (cells)','FontSize',40);
+ylabel('Clock (a.u.)','FontSize',40, 'Color', 'r', 'FontWeight', 'bold');
+
+
+
+figure
+
+plot(PErk_mf(2:end,mf),'o','LineWidth',2,...
+    'MarkerSize',10,...
+    'MarkerEdgeColor',[0.1,0.8,0.1],...
+    'MarkerFaceColor',[0.4,0.8,0.4])
+ax = gca;
+ax.FontSize = 30;
+ax.FontWeight='bold';
+xlabel('PSM (cells)','FontSize',40);
+ylabel('ppERK (a.u.)','FontSize',40, 'Color', [0.1,0.8,0.1], 'FontWeight', 'bold');
+ylim([0 30]);
+
+figure
+plot(PErk_mf(2:end,mf+stp),'o','LineWidth',2,...
+    'MarkerSize',10,...
+    'MarkerEdgeColor',[0.1,0.8,0.1],...
+    'MarkerFaceColor',[0.1,0.5,0.1])
+ax = gca;
+ax.FontSize = 30;
+ax.FontWeight='bold';
+xlabel('PSM (cells)','FontSize',40);
+ylabel('ppERK (a.u.)','FontSize',40, 'Color', [0.1,0.8,0.1], 'FontWeight', 'bold');
+ylim([0 30]);
+
+figure
+plot(PErk_mf(2:end,mf+stp),'o','LineWidth',2,...
+    'MarkerSize',10,...
+    'MarkerEdgeColor',[0.1,0.8,0.1],...
+    'MarkerFaceColor',[0.1,0.5,0.1])
+ax = gca;
+ax.FontSize = 30;
+ax.FontWeight='bold';
+xlabel('PSM (cells)','FontSize',40);
+ylabel('ppERK (a.u.)','FontSize',40, 'Color', [0.1,0.8,0.1], 'FontWeight', 'bold');
+ylim([0 30]);
+
+figure
+yyaxis left
+plot(Dusp4_mf(2:end,mf),'o','LineWidth',2,...
+    'MarkerSize',10,...
+    'MarkerEdgeColor',[1.0,0.1,1.0],...
+    'MarkerFaceColor',[1.0,0.7,1.0]);
+
+ax = gca;
+ax.YColor= 'k';
+ax.FontSize = 30;
+ax.FontWeight='bold';
+ylabel('Dusp4 (a.u.)','FontSize',40, 'Color', [1.0,0.1,1.0], 'FontWeight', 'bold');
+ylim([0 6]);
+
+yyaxis right
+plot(Dusp6_mf(2:end,mf),'o','LineWidth',2,...
+    'MarkerSize',10,...
+    'MarkerEdgeColor',[1.0,0.45,0.1],...
+    'MarkerFaceColor',[1.0,0.75,0.1]);
+
+ax = gca;
+ax.FontWeight='bold';
+xlabel('PSM (cells)','FontSize',40);
+ax.YColor= 'k';
+ylabel('Dusp6 (a.u.)','FontSize',40, 'Color', [1.0,0.45,0.1], 'FontWeight', 'bold');
+ylim([0 6]);
+
+figure
+yyaxis left
+plot(Dusp4_mf(2:end,mf+stp),'o','LineWidth',2,...
+    'MarkerSize',10,...
+    'MarkerEdgeColor',[1.0,0.1,1.0],...
+    'MarkerFaceColor',[0.7,0.3,0.7]);
+
+ax = gca;
+ax.YColor= 'k';
+ax.FontSize = 30;
+ax.FontWeight='bold';
+ylabel('Dusp4 (a.u.)','FontSize',40, 'Color', [1.0,0.1,1.0], 'FontWeight', 'bold');
+ylim([0 6]);
+
+yyaxis right
+plot(Dusp6_mf(2:end,mf+stp),'o','LineWidth',2,...
+    'MarkerSize',10,...
+    'MarkerEdgeColor',[1.0,0.45,0.1],...
+    'MarkerFaceColor',[1.0,0.25,0.1]);
+
+
+ax = gca;
+ax.FontWeight='bold';
+ax.YColor= 'k';
+xlabel('PSM (cells)','FontSize',40);
+
+ylabel('Dusp6 (a.u.)','FontSize',40, 'Color', [1.0,0.45,0.1], 'FontWeight', 'bold');
+ylim([0 6]);
+
+
+
+
+
+
+%{
 hold on
 plot(Tclock(2:end,mf+stp))
 hold on
@@ -193,15 +308,6 @@ hold on
 plot(Dusp4_mf(2:end,mf+3*stp))
 
 
-
-
-plot(SFC(2:end,mf))
-hold on
-plot(SFC(2:end,mf+stp))
-hold on
-plot(SFC(2:end,mf+2*stp))
-hold on
-plot(SFC(2:end,mf+3*stp))
 
 figure
 
@@ -226,9 +332,10 @@ hold on
 plot(Tclock(2:end,mf+3*stp))
 
 %}
+%}
 
 % create the video writer with 1 fps
-  writerObj = VideoWriter('plotmovies.avi');
+  writerObj = VideoWriter('plotmovies2.avi');
   writerObj.FrameRate = 10;
   % set the seconds per image
 % open the video writer
@@ -237,22 +344,23 @@ open(writerObj);
 for i=100/delt:1/delt:300/delt
        
     time=sprintf('time=%.2f mins',i*delt);
-    subplot(2,2,1);
-    plot(Tclock(2:end,i),'b');
-    ylim([0 yC]); xlim([-1 length(xsize)]);ylabel('Clock','fontsize',15);
+    subplot(3,1,1);
+    plot(Tclock(2:end,i),'m');
+    ylim([0 yC]);xlabel('cell #','fontsize',20); xlim([-1 length(xsize)]);ylabel('Clock','fontsize',15);
     title(time,'fontsize',10);
-    subplot(2,2,2);
-    %%plot(PErk_mf(2:end,i));ylim([0 yP]);ylabel('PErk','fontsize',15);
-    plot(PErk_mf(2:end,i));hold on; plot(SFC(2:end,i)); ylim([0 yP]);legend('ppERK','SFC of ppERK','fontsize',10);legend box off;
-    ylabel('ppERK','fontsize',15);xlabel('cell #','fontsize',20);xlim([-1 length(xsize)]);
-    hold off;
-    subplot(2,2,3);
-    plot(PCOMP_mf(2:end,i));ylim([0 yM]);ylabel('PMek','fontsize',15);xlabel('cell #','fontsize',20);xlim([-1 length(xsize)]);
-    subplot(2,2,4);
+
+    subplot(3,1,2);
     plot(Dusp4_mf(2:end,i));hold on; plot(Dusp6_mf(2:end,i)); ylim([0 yD4]);legend('Dusp4','Dusp6','fontsize',10);legend box off;
     ylabel('Dusp','fontsize',15);xlabel('cell #','fontsize',20);xlim([-1 length(xsize)]);
     
     hold off;
+    
+    subplot(3,1,3);
+    plot(PErk_mf(2:end,i),'g'); 
+    ylim([0 yP]);
+    legend('ppERK','fontsize',10);legend box off;
+    ylabel('ppERK','fontsize',15);xlabel('cell #','fontsize',20);xlim([-1 length(xsize)]);
+    
     %%pause(.1);
     frame = getframe(gcf);
     drawnow
